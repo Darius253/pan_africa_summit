c3ea37d7a3813e91c70bab1684df288f8999bb3e
Darius253 <trondarius13@gmail.com>
2025-04-18 01:39:20 +0100

    Refactor registration and authentication screens for improved state management and controller handling

--- lib/ui/onboarding_screens/widgets/shimmer_text.dart
+++ lib/ui/onboarding_screens/widgets/shimmer_text.dart
@@ -2,15 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
 
 class ShimmerText extends StatelessWidget {
-  const ShimmerText({super.key, required this.text});
+  const ShimmerText({super.key, required this.text, required this.theme});
   final String text;
+  final ThemeData theme;
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
     return Shimmer.fromColors(
       baseColor: Color(0xffF561FA),
       highlightColor: Color(0xff2987F2),
+      period: const Duration(milliseconds: 500),
       child: Text(text, style: theme.textTheme.titleLarge),
     );
   }
