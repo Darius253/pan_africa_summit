ed485c91f5d5386ec4213834bda8b79e6ad50cc3
Darius253 <trondarius13@gmail.com>
2025-04-18 01:40:19 +0100

    Refactor registration and authentication screens for improved state management and controller handling

--- lib/ui/authentication_pages/designate_registration/travel_information.dart
+++ lib/ui/authentication_pages/designate_registration/travel_information.dart
@@ -1,25 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:pan_african_ai_summit/ui/authentication_pages/widgets/travel_info_with_checkbox.dart';
+import 'package:pan_african_ai_summit/ui/authentication_pages/widgets/travel_info_with_radio_buttons.dart';
 
-class TravelInformation extends StatefulWidget {
-  const TravelInformation({super.key});
+class TravelInformation extends StatelessWidget {
+  const TravelInformation({super.key, required this.scrollController});
+  final ScrollController scrollController;
 
-  @override
-  State<TravelInformation> createState() => _TravelInformationState();
-}
-
-class _TravelInformationState extends State<TravelInformation> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.start,
-      spacing: 10,
-      children: [
-        Text(
-          "Travel & Special Requirements",
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        spacing: 10,
+        children: [
+          Text(
+            "Travel & Special Requirements",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          TravelInfoWithRadioButtons(
+            question: "Do you require a Visa on Arrival?",
+          ),
+          TravelInfoWithRadioButtons(
+            question: "Do you need assistance with your Visa Application?",
+          ),
+          TravelInfoWithRadioButtons(
+            question: "Do you have any Dietary Requirements?",
+          ),
+
+          TravelInfoWithCheckbox(
+            question: "Accessibility Requirements",
+            options: [
+              "Wheelchair Access",
+              "Sign Language Interpretation",
+              "None",
+            ],
+          ),
+          SizedBox(height: 60),
+        ],
+      ),
     );
   }
 }
