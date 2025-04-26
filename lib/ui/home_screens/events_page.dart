de08db8d8d8e4979adcc9468e02ee4b9c8b9f0e3
Darius253 <trondarius13@gmail.com>
2025-04-26 02:08:54 +0100

    feat: add EventsPage widget for displaying event details and agenda

---
+++ lib/ui/home_screens/events_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:pan_african_ai_summit/ui/home_screens/widgets/events_page_list_tile.dart';
+import 'package:pan_african_ai_summit/ui/utils/gradient_text.dart';
+
+class EventsPage extends StatelessWidget {
+  const EventsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = ScrollController();
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: ListView(
+        // physics: const NeverScrollableScrollPhysics(),
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        controller: controller,
+        children: [
+          const GradientText(
+            text: "Meet the Visionaries Shaping Africa's AI Future",
+            gradient: LinearGradient(
+              colors: [Color(0xff2987F2), Color(0xffF561FA)],
+            ),
+          ),
+          SizedBox(height: 20),
+          const Text(
+            "Connect with global industry leaders to build safe “glocal” ai ecosystem and take your business to the next level.",
+
+            style: TextStyle(color: Colors.white),
+          ),
+          const SizedBox(height: 20),
+          AnimatedListTile(text: "View Full Agenda"),
+          const SizedBox(height: 20),
+          AnimatedListTile(text: "View Speakers"),
+        ],
+      ),
+    );
+  }
+}
