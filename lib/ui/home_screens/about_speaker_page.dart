36768b5d3f029bad54173f68efcb8029e1ce74cb
Darius253 <trondarius13@gmail.com>
2025-04-30 23:15:41 +0100

    feat: add hero animation to AboutSpeakerPage and require heroTag parameter

--- lib/ui/home_screens/about_speaker_page.dart
+++ lib/ui/home_screens/about_speaker_page.dart
@@ -2,44 +2,45 @@ import 'package:flutter/material.dart';
 import 'package:pan_african_ai_summit/ui/utils/gradient_text.dart';
 
 class AboutSpeakerPage extends StatelessWidget {
-  const AboutSpeakerPage({super.key});
+  const AboutSpeakerPage({super.key, required this.heroTag});
+  final String heroTag;
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return Hero(
-      tag: 'speaker',
-      child: Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          actions: [
-            IconButton(
-              icon: Container(
-                width: 50,
-                height: 50,
-                decoration: BoxDecoration(
-                  color: theme.colorScheme.primary,
-                  shape: BoxShape.circle,
-                ),
-                child: Icon(Icons.close, color: theme.colorScheme.onPrimary),
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+            icon: Container(
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                color: theme.colorScheme.primary,
+                shape: BoxShape.circle,
               ),
-              onPressed: () {
-                Navigator.pop(context);
-              },
+              child: Icon(Icons.close, color: theme.colorScheme.onPrimary),
             ),
-          ],
-        ),
-        body: SafeArea(
-          child: ListView(
-            padding: const EdgeInsets.all(20),
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.start,
-                spacing: 5,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(right: 16),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.all(20),
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              spacing: 5,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(right: 16),
+                  child: Hero(
+                    tag: heroTag,
                     child: CircleAvatar(
                       radius: 70,
                       foregroundImage: AssetImage(
@@ -47,49 +48,49 @@ class AboutSpeakerPage extends StatelessWidget {
                       ),
                     ),
                   ),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      spacing: 5,
-                      children: [
-                        GradientText(
-                          text: "Speaker Name",
-                          style: theme.textTheme.titleLarge?.copyWith(
-                            fontWeight: FontWeight.bold,
-                          ),
-                          gradient: LinearGradient(
-                            colors: [Color(0xff2987F2), Color(0xffF561FA)],
-                          ),
-                        ),
-
-                        Text(
-                          "Founder & CEO at DecisiveAI",
-                          style: theme.textTheme.bodyLarge?.copyWith(),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-
-              const SizedBox(height: 20),
-              Text(
-                "About the Speaker",
-                style: theme.textTheme.titleLarge?.copyWith(
-                  fontWeight: FontWeight.bold,
                 ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    spacing: 5,
+                    children: [
+                      GradientText(
+                        text: "Speaker Name",
+                        style: theme.textTheme.titleLarge?.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                        gradient: LinearGradient(
+                          colors: [Color(0xff2987F2), Color(0xffF561FA)],
+                        ),
+                      ),
+
+                      Text(
+                        "Founder & CEO at DecisiveAI",
+                        style: theme.textTheme.bodyLarge?.copyWith(),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 20),
+            Text(
+              "About the Speaker",
+              style: theme.textTheme.titleLarge?.copyWith(
+                fontWeight: FontWeight.bold,
               ),
-              const SizedBox(height: 10),
-              const Text("""
-          Dr. Ghita Mezzour is the Founder & CEO of DecisiveAI, a company that supports public and private organizations in the strategic adoption of artificial intelligence to achieve concrete and sustainable outcomes. She served as Morocco’s Minister of Digital Transition and Administrative Reform (2021–2024), where she led key initiatives that boosted digital education, attracted global tech companies, and launched the Digital Morocco 2030 Strategy.
-          
-          Before her ministerial role, she founded Data in Seconds (DASEC), an award-winning startup using AI to analyze alternative data for strategic insights. She was also an Associate Professor at the International University of Rabat, leading AI projects with international partners like USAID and NATO.
-          
-          Ms. Mezzour holds a PhD in Electrical & Computer Engineering from Carnegie Mellon University (United States) and a master’s in communication systems from EPFL (Switwerland). She received the EPFL Alumni Award (2023) and was named an MIT Rising Star (2015).
-          """),
-            ],
-          ),
+            ),
+            const SizedBox(height: 10),
+            const Text("""
+        Dr. Ghita Mezzour is the Founder & CEO of DecisiveAI, a company that supports public and private organizations in the strategic adoption of artificial intelligence to achieve concrete and sustainable outcomes. She served as Morocco’s Minister of Digital Transition and Administrative Reform (2021–2024), where she led key initiatives that boosted digital education, attracted global tech companies, and launched the Digital Morocco 2030 Strategy.
+        
+        Before her ministerial role, she founded Data in Seconds (DASEC), an award-winning startup using AI to analyze alternative data for strategic insights. She was also an Associate Professor at the International University of Rabat, leading AI projects with international partners like USAID and NATO.
+        
+        Ms. Mezzour holds a PhD in Electrical & Computer Engineering from Carnegie Mellon University (United States) and a master’s in communication systems from EPFL (Switwerland). She received the EPFL Alumni Award (2023) and was named an MIT Rising Star (2015).
+        """),
+          ],
         ),
       ),
     );
