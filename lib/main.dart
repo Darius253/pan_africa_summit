ab920b21cedb3be4263df443af61366ff1ed2f02
Darius253 <trondarius13@gmail.com>
2025-05-06 23:08:59 +0100

    feat: initialize Firebase in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:pan_african_ai_summit/firebase_options.dart';
 import 'package:pan_african_ai_summit/theme.dart';
 import 'package:pan_african_ai_summit/ui/onboarding_screens/onboarding_screen.dart';
 import 'package:pan_african_ai_summit/util.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
