51f4ae634a2a683b576c541f448b1c3ddf3ef5b0
Darius253 <trondarius13@gmail.com>
2025-05-07 01:56:28 +0100

    feat: add signOut method and isSignedOut property to AuthenticationViewModel

--- lib/ui/onboarding_screens/authentication/authentication_view_model.dart
+++ lib/ui/onboarding_screens/authentication/authentication_view_model.dart
@@ -6,10 +6,12 @@ class AuthenticationViewModel extends ChangeNotifier {
 
   bool _isLoading = false;
   bool _isSignedIn = false;
+  bool _isSignedOut = false;
   String? _errorMessage;
 
   bool get isLoading => _isLoading;
   bool get isSignedIn => _isSignedIn;
+  bool get isSignedOut => _isSignedOut;
   String? get errorMessage => _errorMessage;
 
   Future<void> signInWithGoogle() async {
@@ -18,6 +20,21 @@ class AuthenticationViewModel extends ChangeNotifier {
       await _authRepo.signInWithGoogle();
       _errorMessage = null;
       _isSignedIn = true;
+    } catch (e) {
+      _errorMessage = e.toString();
+    } finally {
+      _setLoading(false);
+      notifyListeners();
+    }
+  }
+
+  Future<void> signOut() async {
+    _setLoading(true);
+    try {
+      await _authRepo.signOut();
+      _isSignedOut = true;
+      _isSignedIn = false;
+      _errorMessage = null;
     } catch (e) {
       _errorMessage = e.toString();
     } finally {
