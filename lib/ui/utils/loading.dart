1258c3c00473f879df98ceaf8379112fd41ad9b5
Darius253 <trondarius13@gmail.com>
2025-05-07 01:18:41 +0100

    refactor: rename LoadingPage to Loading and adjust widget properties

--- lib/ui/utils/loading.dart
+++ lib/ui/utils/loading.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
-class LoadingPage extends StatelessWidget {
-  const LoadingPage({super.key});
+class Loading extends StatelessWidget {
+  const Loading({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: double.infinity,
+      height: double.infinity,
       color: const Color.fromARGB(169, 0, 0, 0),
       child: Center(child: CircularProgressIndicator.adaptive(strokeWidth: 5)),
     );
