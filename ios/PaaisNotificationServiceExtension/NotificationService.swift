525d402b89884d18764a75798ea311e8e0a3943f
Darius253 <trondarius13@gmail.com>
2025-05-16 00:02:31 +0100

    refactor: update NotificationService to extend DartAwesomeServiceExtension

--- ios/PaaisNotificationServiceExtension/NotificationService.swift
+++ ios/PaaisNotificationServiceExtension/NotificationService.swift
@@ -6,30 +6,32 @@
 //
 
 import UserNotifications
+import awesome_notifications_fcm
 
-class NotificationService: UNNotificationServiceExtension {
+@available(iOS 10.0, *)
+class NotificationService: DartAwesomeServiceExtension {
 
-    var contentHandler: ((UNNotificationContent) -> Void)?
-    var bestAttemptContent: UNMutableNotificationContent?
+    // var contentHandler: ((UNNotificationContent) -> Void)?
+    // var bestAttemptContent: UNMutableNotificationContent?
 
-    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
-        self.contentHandler = contentHandler
-        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+    // override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+    //     self.contentHandler = contentHandler
+    //     bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
         
-        if let bestAttemptContent = bestAttemptContent {
-            // Modify the notification content here...
-            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
+    //     if let bestAttemptContent = bestAttemptContent {
+    //         // Modify the notification content here...
+    //         bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
             
-            contentHandler(bestAttemptContent)
-        }
-    }
+    //         contentHandler(bestAttemptContent)
+    //     }
+    // }
     
-    override func serviceExtensionTimeWillExpire() {
-        // Called just before the extension will be terminated by the system.
-        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
-            contentHandler(bestAttemptContent)
-        }
-    }
+    // override func serviceExtensionTimeWillExpire() {
+    //     // Called just before the extension will be terminated by the system.
+    //     // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+    //     if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+    //         contentHandler(bestAttemptContent)
+    //     }
+    // }
 
 }
