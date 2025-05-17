ccfd382563a80177534371493877219e3716b038
Darius253 <trondarius13@gmail.com>
2025-05-17 01:19:45 +0100

    feat: refactor AwesomeNotifications initialization and enhance MyApp structure

--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,79 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pan_african_ai_summit/firebase_options.dart';
 import 'package:pan_african_ai_summit/theme.dart';
+import 'package:pan_african_ai_summit/ui/notification_controller.dart';
 import 'package:pan_african_ai_summit/util.dart';
 import 'package:pan_african_ai_summit/ui/wrapper.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  AwesomeNotifications().initialize(
+  await _initialiseAwesomeNotifications();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+  static final GlobalKey<NavigatorState> navigatorKey =
+      GlobalKey<NavigatorState>();
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    // Only after at least the action method is set, the notification events are delivered
+    AwesomeNotifications().setListeners(
+      onActionReceivedMethod: NotificationController.onActionReceivedMethod,
+      onNotificationCreatedMethod:
+          NotificationController.onNotificationCreatedMethod,
+      onNotificationDisplayedMethod:
+          NotificationController.onNotificationDisplayedMethod,
+      onDismissActionReceivedMethod:
+          NotificationController.onDismissActionReceivedMethod,
+    );
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final brightness = View.of(context).platformDispatcher.platformBrightness;
+    TextTheme textTheme = createTextTheme(context, "Poppins", "Lato");
+
+    MaterialTheme theme = MaterialTheme(textTheme);
+    return MaterialApp(
+      navigatorKey: MyApp.navigatorKey,
+      title: 'Pan African AI Summit',
+      initialRoute: '/',
+      onGenerateRoute: (settings) {
+        return MaterialPageRoute(builder: (context) => Wrapper());
+      },
+
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+
+      theme: brightness == Brightness.light ? theme.light() : theme.dark(),
+
+      home: Wrapper(),
+    );
+  }
+}
+
+Future<void> _initialiseAwesomeNotifications() async {
+  await AwesomeNotifications().initialize(
     // set the icon to null if you want to use the default app icon
-    'resource://drawable/res_app_icon',
+    null,
     [
       NotificationChannel(
         channelGroupKey: 'basic_channel_group',
         channelKey: 'basic_channel',
         channelName: 'Basic notifications',
         channelDescription: 'Notification channel for basic tests',
-        defaultColor: Color(0xFF9D50DD),
+        defaultColor: Colors.transparent,
         ledColor: Colors.white,
       ),
     ],
@@ -31,26 +88,4 @@ void main() async {
     ],
     debug: true,
   );
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final brightness = View.of(context).platformDispatcher.platformBrightness;
-    TextTheme textTheme = createTextTheme(context, "Poppins", "Lato");
-
-    MaterialTheme theme = MaterialTheme(textTheme);
-    return MaterialApp(
-      title: 'Pan African AI Summit',
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.system,
-
-      theme: brightness == Brightness.light ? theme.light() : theme.dark(),
-
-      home: Wrapper(),
-    );
-  }
 }
