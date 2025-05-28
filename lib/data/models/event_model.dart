789d517445d3478570b1a7e3cc417fa4dbc63e94
Darius253 <trondarius13@gmail.com>
2025-05-29 00:00:59 +0100

    fix: remove unused speakers field from EventModel

--- lib/data/models/event_model.dart
+++ lib/data/models/event_model.dart
@@ -1,5 +1,4 @@
 import 'package:pan_african_ai_summit/data/models/agenda_model.dart';
-import 'package:pan_african_ai_summit/data/models/speaker_model.dart';
 
 class EventModel {
   String? headline;
@@ -8,7 +7,6 @@ class EventModel {
   String? startDate;
   String? endDate;
   String? subtitle;
-  List<SpeakerModel>? speakers;
   List<AgendaModel>? agenda;
 
   EventModel({
@@ -18,7 +16,7 @@ class EventModel {
     this.startDate,
     this.endDate,
     this.subtitle,
-    this.speakers,
+  
     this.agenda,
   });
   factory EventModel.fromJson(Map<String, dynamic> json) {
@@ -29,26 +27,11 @@ class EventModel {
       startDate: json['start_date'] as String?,
       endDate: json['end_date'] as String?,
       subtitle: json['subtitle'] as String?,
-      speakers:
-          (json['speakers'] as List<dynamic>?)
-              ?.map((e) => SpeakerModel.fromJson(e))
-              .toList(),
+      
       agenda:
           (json['agenda'] as List<dynamic>?)
               ?.map((e) => AgendaModel.fromJson(e))
               .toList(),
     );
-  }
-  Map<String, dynamic> toJson() {
-    return {
-      'headline': headline,
-      'description': description,
-      'location': location,
-      'start_date': startDate,
-      'end_date': endDate,
-      'subtitle': subtitle,
-      'speakers': speakers?.map((e) => e.toJson()).toList(),
-      'agenda': agenda?.map((e) => e.toJson()).toList(),
-    };
   }
 }
