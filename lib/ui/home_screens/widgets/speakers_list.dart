ac5b0e56961042c2dcad9b343326f7bb2fac1ff2
Darius253 <trondarius13@gmail.com>
2025-05-30 01:20:00 +0100

    refactor: update SpeakersList to use SpeakersViewModel and improve speaker filtering

--- lib/ui/home_screens/widgets/speakers_list.dart
+++ lib/ui/home_screens/widgets/speakers_list.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:pan_african_ai_summit/data/models/speaker_model.dart';
 import 'package:pan_african_ai_summit/ui/events_resgistration/widgets/primary_button.dart';
 import 'package:pan_african_ai_summit/ui/home_screens/about_speaker_page.dart';
-import 'package:pan_african_ai_summit/ui/home_screens/events_page/events_view_model.dart';
+import 'package:pan_african_ai_summit/ui/home_screens/events_page/speakers_view_model.dart';
 import 'package:pan_african_ai_summit/ui/utils/gradient_text.dart';
 
 class SpeakersList extends StatefulWidget {
-  const SpeakersList({super.key});
+  final SpeakersViewModel viewModel;
+  const SpeakersList({super.key, required this.viewModel});
 
   @override
   State<SpeakersList> createState() => _SpeakersListState();
@@ -15,8 +15,6 @@ class SpeakersList extends StatefulWidget {
 class _SpeakersListState extends State<SpeakersList>
     with AutomaticKeepAliveClientMixin {
   final _scrollController = ScrollController();
-  final _viewModel = EventsViewModel();
-  List<SpeakerModel> _speakers = [];
 
   @override
   void initState() {
@@ -28,10 +26,7 @@ class _SpeakersListState extends State<SpeakersList>
   bool get wantKeepAlive => true;
 
   void _loadSpeakers() async {
-    final speakers = await _viewModel.fetchSpeakers();
-    setState(() {
-      _speakers = speakers;
-    });
+    await widget.viewModel.fetchSpeakers();
   }
 
   @override
@@ -45,14 +40,16 @@ class _SpeakersListState extends State<SpeakersList>
     super.build(context);
     final theme = Theme.of(context);
     return ListenableBuilder(
-      listenable: _viewModel,
+      listenable: widget.viewModel,
       builder: (context, _) {
-        if (_viewModel.isLoading) {
-          return const Center(child: CircularProgressIndicator());
+        if (widget.viewModel.isLoading) {
+          return const Center(child: CircularProgressIndicator.adaptive());
         }
-        if (_speakers.isEmpty) {
+        if (widget.viewModel.speakers.isEmpty &&
+            widget.viewModel.filteredSpeakers.isEmpty) {
           return const Center(child: Text("No speakers available"));
         }
+
         return GridView.builder(
           key: const PageStorageKey<String>("speakers"),
           physics: const AlwaysScrollableScrollPhysics(),
@@ -63,30 +60,68 @@ class _SpeakersListState extends State<SpeakersList>
             mainAxisExtent: 300,
           ),
           itemBuilder: (_, index) {
-            return _buildSpeakersCard(
-              theme: theme,
-              imageUrl: _speakers[index].imageUrl,
-              name: _speakers[index].name,
-              bio: _speakers[index].bio,
-              onPressed:
-                  () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder:
-                          (context) => AboutSpeakerPage(
-                            heroTag: "speaker$index",
-                            imageUrl: _speakers[index].imageUrl,
-                            speakerName: _speakers[index].name,
-                            bio: _speakers[index].bio,
-                          ),
-                    ),
-                  ),
-              heroTag: "speaker$index",
-            );
+            return widget.viewModel.filteredSpeakers.isNotEmpty
+                ? _buildSpeakersCard(
+                  theme: theme,
+                  imageUrl: widget.viewModel.filteredSpeakers[index].imageUrl,
+                  name: widget.viewModel.filteredSpeakers[index].name,
+                  bio: widget.viewModel.filteredSpeakers[index].bio,
+                  onPressed:
+                      () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder:
+                              (context) => AboutSpeakerPage(
+                                heroTag: "speaker$index",
+                                imageUrl:
+                                    widget
+                                        .viewModel
+                                        .filteredSpeakers[index]
+                                        .imageUrl,
+                                speakerName:
+                                    widget
+                                        .viewModel
+                                        .filteredSpeakers[index]
+                                        .name,
+                                bio:
+                                    widget
+                                        .viewModel
+                                        .filteredSpeakers[index]
+                                        .bio,
+                              ),
+                        ),
+                      ),
+                  heroTag: "speaker$index",
+                )
+                : _buildSpeakersCard(
+                  theme: theme,
+                  imageUrl: widget.viewModel.speakers[index].imageUrl,
+                  name: widget.viewModel.speakers[index].name,
+                  bio: widget.viewModel.speakers[index].bio,
+                  onPressed:
+                      () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder:
+                              (context) => AboutSpeakerPage(
+                                heroTag: "speaker$index",
+                                imageUrl:
+                                    widget.viewModel.speakers[index].imageUrl,
+                                speakerName:
+                                    widget.viewModel.speakers[index].name,
+                                bio: widget.viewModel.speakers[index].bio,
+                              ),
+                        ),
+                      ),
+                  heroTag: "speaker$index",
+                );
           },
 
           controller: _scrollController,
-          itemCount: _speakers.length,
+          itemCount:
+              widget.viewModel.filteredSpeakers.isNotEmpty
+                  ? widget.viewModel.filteredSpeakers.length
+                  : widget.viewModel.speakers.length,
           shrinkWrap: true,
         );
       },
