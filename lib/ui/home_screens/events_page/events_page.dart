09b18b74570ca0a005a1e6b02abe255969ba7be0
Darius253 <trondarius13@gmail.com>
2025-05-30 01:19:36 +0100

    refactor: update EventsPage layout and enhance agenda display

--- lib/ui/home_screens/events_page/events_page.dart
+++ lib/ui/home_screens/events_page/events_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pan_african_ai_summit/ui/events_resgistration/widgets/primary_button.dart';
-import 'package:pan_african_ai_summit/ui/home_screens/widgets/events_page_list_tile.dart';
+import 'package:pan_african_ai_summit/ui/home_screens/events_page/speakers_view_model.dart';
+import 'package:pan_african_ai_summit/ui/home_screens/widgets/animated_list_event_page.dart.dart';
 import 'package:pan_african_ai_summit/ui/home_screens/widgets/speakers_list.dart';
 import 'package:pan_african_ai_summit/ui/utils/gradient_text.dart';
 
@@ -11,69 +12,77 @@ class EventsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: ListView(
-        scrollDirection: Axis.vertical,
-        shrinkWrap: true,
-        controller: controller,
-        children: [
-          const GradientText(
-            text: "Meet the Visionaries Shaping Africa's AI Future",
-            gradient: LinearGradient(
-              colors: [Color(0xff2987F2), Color(0xffF561FA)],
+    final viewModel = SpeakersViewModel();
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          controller: controller,
+          children: [
+            const GradientText(
+              text: "Meet the Visionaries Shaping Africa's AI Future",
+              gradient: LinearGradient(
+                colors: [Color(0xff2987F2), Color(0xffF561FA)],
+              ),
             ),
-          ),
-          SizedBox(height: 20),
-          const Text(
-            "Connect with global industry leaders to build safe “glocal” ai ecosystem and take your business to the next level.",
-            style: TextStyle(color: Colors.white),
-          ),
-          const SizedBox(height: 20),
-          AnimatedListTile(
-            text: "View Full Agenda",
-            isAgenda: true,
-            eventDays: SizedBox(
-              height: 50,
+            SizedBox(height: 20),
+            const Text(
+              "Stay tuned as we reveal an exciting lineup of some of the brightest minds in AI. Check back regularly for updates and get ready to be inspired!",
+              style: TextStyle(color: Colors.white),
+            ),
+            const SizedBox(height: 20),
+            AnimatedListTile(
+              text: "View Full Agenda",
+              isAgenda: true,
+              eventDays: SizedBox(
+                height: 50,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (_, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: PrimaryButton(
+                        onPressed: () {},
+                        color: theme.colorScheme.tertiaryContainer,
+                        text: "Day ${index + 1}",
+                        textColor: theme.colorScheme.onTertiaryContainer,
+                      ),
+                    );
+                  },
+                  itemCount: 2,
+                  shrinkWrap: true,
+                ),
+              ),
+              viewModel: viewModel,
               child: ListView.builder(
-                scrollDirection: Axis.horizontal,
+                physics: const AlwaysScrollableScrollPhysics(),
                 itemBuilder: (_, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: PrimaryButton(
-                      onPressed: () {},
-                      color: theme.colorScheme.tertiaryContainer,
-                      text: "Day ${index + 1}",
-                      textColor: theme.colorScheme.onTertiaryContainer,
-                    ),
+                  return _buildAgendaTile(
+                    theme: theme,
+                    agenda: "Registration and Networking",
+                    time: "8:00AM - 10:00 AM",
+                    description:
+                        "Join us for an insightful session on the impact of AI in the finance sector. Learn from industry experts and discover the latest trends and technologies.",
                   );
                 },
-                itemCount: 2,
+                itemCount: 10,
                 shrinkWrap: true,
               ),
             ),
-            child: ListView.builder(
-              physics: const AlwaysScrollableScrollPhysics(),
-              itemBuilder: (_, index) {
-                return _buildAgendaTile(
-                  theme: theme,
-                  agenda: "Registration and Networking",
-                  time: "8:00AM - 10:00 AM",
-                  description:
-                      "Join us for an insightful session on the impact of AI in the finance sector. Learn from industry experts and discover the latest trends and technologies.",
-                );
-              },
-              itemCount: 10,
-              shrinkWrap: true,
+            const SizedBox(height: 20),
+            AnimatedListTile(
+              text: "View Speakers",
+              isAgenda: false,
+              viewModel: viewModel,
+              child: SpeakersList(viewModel: viewModel),
             ),
-          ),
-          const SizedBox(height: 20),
-          AnimatedListTile(
-            text: "View Speakers",
-            isAgenda: false,
-            child: SpeakersList(),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
